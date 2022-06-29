4970e6b8af3fec5301388f67690020de2a63ec8d
Loskir <kl@sinker.ru>
2022-06-29 19:45:10 +0300

    Update styles

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="product__container m-8">
+  <div class="flex flex-col">
     <img class="product__image mb-4" v-if="product.photoUrl" :src="product.photoUrl" alt="Photo" />
     <h1 class="product__name text-4xl font-semibold">{{ product.name }}</h1>
     <h2 class="product__price">
       <span class="product__discount-price font-bold text-red-600 text-xl">{{ product.price }}₽</span>
       ({{ product.basePrice }}₽)
     </h2>
-    <p class="product__ean">Арт. {{ product.ean }}</p>
+    <p class="text-gray-500">Арт. {{ product.ean }}</p>
   </div>
 </template>
 
@@ -26,20 +26,8 @@ defineProps<{
 </script>
 
 <style>
-.product__container {
-  display: flex;
-  flex-direction: column;
-}
-
-.product__image-container {
-  max-height: 40vh;
-  width: auto;
-  display: flex;
-  justify-content: center;
-}
-
 .product__image {
-  max-height: 40vh;
+  max-height: 56.25%;
   object-fit: contain;
 }
 </style>
--- src/views/ProductEanView.vue
+++ src/views/ProductEanView.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="absolute inset-12 flex flex-col justify-center" v-if="store.isLoading">
-    <span class="text-center">Loading...</span>
-  </div>
-  <Product v-else-if="store.product" :product="store.product" />
-  <div v-else-if="store.error">{{ store.error }}</div>
-  <div class="absolute inset-12 flex flex-col justify-center" v-else>
-    <span class="text-center">Not found :(</span>
+  <div class="max-w-xl mx-auto p-8">
+    <div class="absolute inset-12 flex flex-col justify-center" v-if="store.isLoading">
+      <span class="text-center">Loading...</span>
+    </div>
+    <Product v-else-if="store.product" :product="store.product" />
+    <div v-else-if="store.error">{{ store.error }}</div>
+    <div class="absolute inset-12 flex flex-col justify-center" v-else>
+      <span class="text-center">Not found :(</span>
+    </div>
   </div>
 </template>
 
