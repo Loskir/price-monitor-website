57c4482bb83244c5e232c6498119c9539bd02d0c
Loskir <kl@sinker.ru>
2022-07-06 16:01:25 +0300

    Update styles

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex flex-col">
     <img class="product__image mb-4" v-if="product.photoUrl" :src="product.photoUrl" alt="Photo" />
-    <h1 class="product__name text-4xl font-semibold">{{ product.name }}</h1>
-    <h2 class="product__price" v-if="product.price">
+    <h1 class="product__name text-2xl font-semibold">{{ product.name }}</h1>
+    <h2 class="pt-1" v-if="product.price">
       <template v-if="product.price.price !== product.price.basePrice">
-        <span class="font-bold text-green-600 text-xl">
+        <span class="font-bold text-green-600 text-2xl">
           {{ product.price.price }}₽
         </span> <s>
           {{ product.price.basePrice }}₽
         </s>
       </template>
       <template v-else>
-        <span class="font-bold text-xl">{{ product.price.price }}₽</span>
+        <span class="font-bold text-2xl">{{ product.price.price }}₽</span>
       </template>
     </h2>
-    <p class="text-gray-500">Арт. {{ product.ean }}</p>
+    <p class="text-gray-500 pt-1">Арт. {{ product.ean }}</p>
     <p class="text-gray-500">ID: {{ product.productId }}</p>
     <p class="text-gray-500" v-if="product.price">Обновлено {{
         DateTime.fromISO(product.price.time).setLocale('ru').toFormat('dd MMMM в HH:mm ZZZZ')
